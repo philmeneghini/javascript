95cf4dbf3ee561ecea6c23c976bdb9a538061423
Philippe Meneghini <philmeneghini@gmail.com>
2019-08-09 01:14:03 -0300

    Construindo a interface web - 30:50

---
+++ semana-omnistack-8/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import Login from "./pages/Login";
+import { directive } from '@babel/types';
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
---
+++ semana-omnistack-8/frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// DOM é a árvore de elementos do HTML
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
