4a12baa1704f6fb65edf23d2fa44019464237cfc
Philippe Meneghini <philmeneghini@gmail.com>
2019-08-10 01:59:53 -0300

    Construindo Interface Web - Finalizado

--- semana-omnistack-8/frontend/src/App.js
+++ semana-omnistack-8/frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Login from "./pages/Login";
-import { directive } from '@babel/types';
+import Routes from "./routes";
 
 function App() {
   return (
-    <Login />
+    <Routes />
   );
 }
 
