6a20e93f179d7b0a49eec9c506050f92b851ec4a
Philippe Meneghini <philmeneghini@gmail.com>
2019-08-15 14:42:22 -0300

    Mobile App Finalizado, faltam advanced func

--- semana-omnistack-8/tindev/src/pages/Login.js
+++ semana-omnistack-8/tindev/src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { View, StyleSheet, Image, TextInput, TouchableOpacity, Text } from 'react-native';
 
 import api from '../services/api';
@@ -7,15 +8,25 @@ import logo from '../assets/logo.png';
 
 export default function Login({ navigation }){
   const [user, setUser] = useState('');
+
+  useEffect( () => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, [] );
   
   async function handleLogin(){
     const response = await api.post('/devs', { username: user });
 
     const { _id } = response.data;
 
+    await AsyncStorage.setItem('user', _id);
+
     console.log(_id);
 
-    navigation.navigate('Main', { _id });
+    navigation.navigate('Main', { user: _id });
   }
   return (  
     <View style = {styles.container}>
