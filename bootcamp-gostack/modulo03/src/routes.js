917072e747443e4607656c19e93d35c0bdcebf74
Philippe Meneghini <philmeneghini@gmail.com>
2019-09-12 01:23:10 -0300

    Listando agenda do prestador - terminar

--- bootcamp-gostack/modulo03/src/routes.js
+++ bootcamp-gostack/modulo03/src/routes.js
@@ -7,8 +7,10 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
+
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single("file"), FileController.store);
 
