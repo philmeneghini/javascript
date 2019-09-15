3023e0e078cfead8506187b9618c2a16c82bfd74
Philippe Meneghini <philmeneghini@gmail.com>
2019-09-15 20:13:42 -0300

    CONFIGURANDO NODEMAILER

--- bootcamp-gostack/modulo03/src/app/controllers/AppointmentController.js
+++ bootcamp-gostack/modulo03/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale';
 import User from '../models/User';
 import File from '../models/File';
@@ -117,6 +117,22 @@ class AppointmentController {
      content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
      user: provider_id,
    });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res){
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
