bb3e3c0f04832d53512a510ee746de3be40d0b40
Philippe Meneghini <philmeneghini@gmail.com>
2019-09-15 20:54:56 -0300

    CONFIGURANDO TEMPLATES DE EMAIL

---
+++ bootcamp-gostack/modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "c89e1bed6e60d4",
+    pass: "7413c0da9e75e7"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
